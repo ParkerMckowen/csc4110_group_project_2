2daceba25cbf12c9e65a7666359cd723582e0482
ParkerMckowen <mckowenparker@gmail.com>
2022-04-04 14:36:27 -0400

    Add basic framework for functionalities

--- barmaster.py
+++ barmaster.py
@@ -17,7 +17,52 @@ def create_window():
     return window
 
 
+def clockIn():
+    return 1
+
+
+def clockOut():
+    return 1
+
+
+def displayInventory():
+    return 1
+
+
+def addInventory():
+    return 1
+
+
+def displaySales():
+    return 1
+
+
+def repeatOrder():
+    return 1
+
+
 if __name__ == "__main__":
 
     window = create_window()
+
+    clockIn = Button(window, text="Clock In", command=clockIn).grid(row=0, column=0)
+
+    clockOut = Button(window, text="Clock Out", command=clockOut).grid(row=1, column=0)
+
+    viewInventory = Button(
+        window, text="View Inventory", command=displayInventory
+    ).grid(row=2, column=0)
+
+    fillInventory = Button(window, text="Add to Inventory", command=addInventory).grid(
+        row=3, column=0
+    )
+
+    viewSales = Button(window, text="View Sales", command=displaySales).grid(
+        row=4, column=0
+    )
+
+    reorder = Button(window, text="Repeat Last Order", command=repeatOrder).grid(
+        row=5, column=0
+    )
+
     window.mainloop()
